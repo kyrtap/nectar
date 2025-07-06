035204c8225d718c7d1db7611ffc1ee499a86249
Patryk Brzoza <kyrtap@users.noreply.github.com>
2025-07-06 15:56:53 +0200

    Init repository

---
+++ src/main/java/de/kyrtap/nectar/NectarApplication.java
@@ -0,0 +1,13 @@
+package de.kyrtap.nectar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NectarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NectarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kyrtap/nectar/NectarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kyrtap.nectar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NectarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
